11f5cfbf93c50d1658d7068cdd1a815998f2fd3f
GillHuang-Xtler <35722674+GillHuang-Xtler@users.noreply.github.com>
2025-07-30 15:28:34 +0200

    Update generate_data_distribution.py

--- generate_data_distribution.py
+++ generate_data_distribution.py
@@ -2,11 +2,7 @@ from loguru import logger
 import pathlib
 import os
 from federated_learning.arguments import Arguments
-from federated_learning.datasets import CIFAR10Dataset
-from federated_learning.datasets import CIFAR100Dataset
 from federated_learning.datasets import FashionMNISTDataset
-from federated_learning.datasets import MNISTDataset
-from federated_learning.datasets import STL10Dataset
 # from federated_learning.datasets import TRECDataset
 from federated_learning.utils import generate_train_loader
 # from federated_learning.utils import generate_benign_loader, generate_malicious_loader, generate_free_loader
